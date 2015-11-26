d08af5808aec1f893e28ec109d6a52505b290edc
Marcio Granzotto Rodrigues <marciogranzotto@msn.com>
2015-11-26 08:44:22 -0200

    Initial commit

---
+++ listenFolder.sh
@@ -0,0 +1,1 @@
+fswatch -0 /Users/marciogranzotto/Documents/3dPrinting/gCodes | xargs -0 -n 1 /Users/marciogranzotto/Documents/3dPrinting/syncToOctopi.sh
---
+++ syncToOctopi.sh
@@ -0,0 +1,2 @@
+rsync -avh /Users/marciogranzotto/Documents/3dPrinting/gCodes/ octopi:.octoprint/uploads/
+echo 'sincronizando pasta!'
